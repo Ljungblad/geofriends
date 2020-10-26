6dbcf313df8b75c5fe34c4a0102ce3937246b3da
Victor Ljungblad <victor.ljungblad@gmail.com>
2020-10-26 14:57:22 +0100

    added delete account and change password logic

--- screens/ChangePasswordScreen/ChangePasswordScreen.js
+++ screens/ChangePasswordScreen/ChangePasswordScreen.js
@@ -1,11 +1,57 @@
 import React, { useState, useEffect } from "react";
 import { Text, View } from "react-native";
+import InputField from "../../components/InputField/InputField";
+import SubmitButton from "../../components/SubmitButton/SubmitButton";
 import styles from "./styles";
+import firebase from "../../FirebaseConfig";
 
 const ChangePasswordScreen = ({ navigation }) => {
+  const [currentPassword, setCurrentPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+
+  const reauthenticate = (currentPassword) => {
+    const user = firebase.auth().currentUser;
+    const credential = firebase.auth.EmailAuthProvider.credential(
+      user.email,
+      currentPassword
+    );
+    return user.reauthenticateWithCredential(credential);
+  };
+
+  const handleChangePassword = () => {
+    reauthenticate(currentPassword)
+      .then(() => {
+        const user = firebase.auth().currentUser;
+        user
+          .updatePassword(newPassword)
+          .then(() => {
+            alert("Password was updated!");
+            navigation.navigate("Profile");
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Change Password Screen</Text>
+      <InputField
+        label="Current password"
+        placeholder="Enter your current password"
+        secureTextEntry={true}
+        onChangeText={(currentPassword) => setCurrentPassword(currentPassword)}
+      />
+      <InputField
+        label="New password"
+        placeholder="Enter your new password"
+        secureTextEntry={true}
+        onChangeText={(newPassword) => setNewPassword(newPassword)}
+      />
+      <SubmitButton label="Change password" onPress={handleChangePassword} />
     </View>
   );
 };
--- screens/MapScreen/MapScreen.js
+++ screens/MapScreen/MapScreen.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { Text, View } from "react-native";
 import MapView, { PROVIDER_GOOGLE, Marker, Callout } from "react-native-maps";
 import * as Location from "expo-location";
-// import data from "../../users.json";
 import styles from "./styles";
 import firebase from "../../FirebaseConfig";
 
