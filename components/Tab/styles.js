5213fc1081bc599dbc8f6307bab22b6f08b7bb63
Michaela Lundborg <54806234+lundborgm@users.noreply.github.com>
2020-10-26 13:48:13 +0100

    Friendscreen + Tab component

---
+++ components/Tab/styles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  tab: {
+    backgroundColor: "#E0E0E0",
+    flex: 0.5, 
+    alignItems: "center", 
+    justifyContent: "center",
+  },
+  active: {
+    backgroundColor: "#9E9E9E",
+  }
+});
--- screens/FriendScreen/FriendScreen.js
+++ screens/FriendScreen/FriendScreen.js
@@ -1,11 +1,44 @@
-import React, { useState, useEffect } from "react";
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+import { Text, View, FlatList } from "react-native";
+import SubmitButton from "../../components/SubmitButton/SubmitButton";
+import Tab from "../../components/Tab/Tab";
 import styles from "./styles";
 
 const FriendScreen = () => {
+  const [tab, setTab] = useState("following");
+  const [activeTab, setActiveTab] = useState(false);
+
   return (
     <View style={styles.container}>
-      <Text>Friend Screen</Text>
+
+      <View style={styles.topBar}>
+        <Tab title="Following" onPress={() => setTab("following")} />
+        <Tab title="Followers" onPress={() => setTab("followers")} />
+      </View>
+
+      {tab === "following" ? (
+        <View>
+          <SubmitButton label="Add Friend" onPress={() => console.log('Add friend')} />
+          <FlatList 
+            style={{marginTop: 40, marginLeft: 20}}
+            data={[
+              {key: 'Devin'},
+              {key: 'Dan'},
+              {key: 'Dominic'},
+              {key: 'Jackson'},
+              {key: 'James'},
+              {key: 'Joel'},
+              {key: 'John'},
+              {key: 'Jillian'},
+              {key: 'Jimmy'},
+              {key: 'Julie'},
+            ]}
+            renderItem={({item}) => <Text style={{fontSize: 18, marginTop: 10}}>{item.key}</Text>}
+          />
+        </View>
+      ) : (
+        <Text>Followers</Text>
+      )}
     </View>
   );
 };
