c4b5afbb2c312ff9dc093896537c19e9d7395ed7
Victor Ljungblad <victor.ljungblad@gmail.com>
2020-11-04 15:06:25 +0100

    added pin modal logic to update database

--- src/components/CreatePinModal/CreatePinModal.js
+++ src/components/CreatePinModal/CreatePinModal.js
@@ -1,11 +1,32 @@
-import React from "react";
-import { View, Text, Button } from "react-native";
+import React, { useState } from "react";
+import { View, Text, TextInput, Button } from "react-native";
 import styles from "./styles";
 import Modal from "react-native-modalbox";
+import SubmitButton from "../SubmitButton/SubmitButton";
+import firebase from "../../../FirebaseConfig";
 
 const CreatePinModal = ({ isOpen, onClosed }) => {
+  const [description, setDescription] = useState("");
+  const currentUserId = firebase.auth().currentUser.uid;
+  const currentUserRef = firebase
+    .firestore()
+    .collection("users")
+    .doc(currentUserId);
+
+  const updatePin = async () => {
+    await currentUserRef.update({
+      "pin.description": description,
+      "pin.isActive": true,
+    });
+  };
+
   return (
-    <Modal style={styles.container} isOpen={isOpen} onClosed={onClosed}>
+    <Modal
+      style={styles.container}
+      isOpen={isOpen}
+      onClosed={onClosed}
+      coverScreen={true}
+    >
       <Text>This is a modal</Text>
       <Button
         style={styles.btn}
@@ -15,6 +36,24 @@ const CreatePinModal = ({ isOpen, onClosed }) => {
           onClosed();
         }}
       ></Button>
+      <View style={styles.inputWrapepr}>
+        <Text style={styles.label}>Description</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="What do you wanna share?"
+          autoCapitalize="none"
+          onChangeText={(description) => setDescription(description)}
+          numberOfLines={10}
+          multiline={true}
+        />
+        <SubmitButton
+          label="Add pin"
+          onPress={() => {
+            updatePin();
+            onClosed();
+          }}
+        />
+      </View>
     </Modal>
   );
 };
--- src/components/CreatePinModal/styles.js
+++ src/components/CreatePinModal/styles.js
@@ -1,13 +1,28 @@
 import { StyleSheet } from "react-native";
+import colors from "../../styles/colors";
 
 export default StyleSheet.create({
   container: {
-    flex: 1,
+    // flex: 1,
     alignItems: "center",
     justifyContent: "center",
+    height: 300,
+    width: 300,
+    borderRadius: 5,
   },
   btn: {
     position: "relative",
     top: 200,
+  },
+  inputWrapper: {},
+  label: { marginBottom: 2 },
+  input: {
+    height: 100,
+    width: 250,
+    backgroundColor: colors.lightGrey,
+    justifyContent: "flex-start",
+    textAlignVertical: "top",
+    paddingLeft: 5,
+    marginBottom: 10,
   },
 });
