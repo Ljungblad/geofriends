e83c9035cc1af49184c47b046d3b28dd8a908519
Michaela Lundborg <54806234+lundborgm@users.noreply.github.com>
2020-11-06 15:47:05 +0100

    updated modal styling

--- src/components/AddFriendModal/AddFriendModal.js
+++ src/components/AddFriendModal/AddFriendModal.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { View, Text, Alert } from "react-native";
 import styles from "./styles";
+import globalStyles from "../../styles/globalStyles";
 import Modal from "react-native-modalbox";
 import InputField from "../InputField/InputField";
 import SubmitButton from "../SubmitButton/SubmitButton";
@@ -8,6 +9,8 @@ import CloseButton from "../CloseButton/CloseButton";
 import firebase from "../../../FirebaseConfig";
 
 const AddFriendModal = ({ isOpen, onClosed, triggerBackdrop }) => {
+  const [error, setError] = useState(false);
+  const errorMsg = "Oops! Couldn't find email. Please try again.";
   const [email, setEmail] = useState("");
   const currentUserId = firebase.auth().currentUser.uid;
   const currentUserRef = firebase
@@ -21,6 +24,7 @@ const AddFriendModal = ({ isOpen, onClosed, triggerBackdrop }) => {
     });
   };
 
+  // !!!! CHECK IF EMAIL EXISTS IN DATABASE !!!!
   const getUserByEmail = async () => {
     await firebase
       .firestore()
@@ -34,6 +38,7 @@ const AddFriendModal = ({ isOpen, onClosed, triggerBackdrop }) => {
         });
       })
       .catch((err) => {
+        console.log(err);
         Alert.alert(
           "Invalid email!",
           "We could not find the user you were looking for."
@@ -43,12 +48,12 @@ const AddFriendModal = ({ isOpen, onClosed, triggerBackdrop }) => {
 
   return (
     <Modal
-      style={styles.container}
+      style={globalStyles.modalContainer}
       isOpen={isOpen}
       onClosed={onClosed}
       coverScreen={true}
     >
-      <View style={styles.buttonContainer}>
+      <View style={globalStyles.closeButtonContainer}>
         <CloseButton
           size={30}
           onPress={() => {
@@ -56,25 +61,30 @@ const AddFriendModal = ({ isOpen, onClosed, triggerBackdrop }) => {
           }}
         />
       </View>
-      <Text>Follow a friend by adding their email.</Text>
-      <InputField
-        label="Email"
-        placeholder="example@mail.com"
-        onChangeText={(email) => setEmail(email)}
-      />
-      <SubmitButton
-        label="Add Friend"
-        onPress={() => {
-          if (email !== "") {
-            getUserByEmail();
-            onClosed();
-            setEmail("");
-            triggerBackdrop();
-          } else {
-            Alert.alert("Field empty", "Please enter a valid email address.");
-          }
-        }}
-      />
+      <View style={globalStyles.inputWrapper}>
+        <Text style={[globalStyles.title, styles.title]}>Follow a friend by adding their email.</Text>
+        <InputField
+          label="Email"
+          placeholder="example@mail.com"
+          onChangeText={(email) => setEmail(email)}
+        />
+        <View style={styles.test}>
+          {error && (<Text style={styles.errorMsg}>{errorMsg}</Text>)}
+        </View>
+        <SubmitButton
+          label="Add Friend"
+          onPress={() => {
+            if (email !== "") {
+              getUserByEmail();
+              onClosed();
+              setEmail("");
+              triggerBackdrop();
+            } else {
+              Alert.alert("Field empty", "Please enter a valid email address.");
+            }
+          }}
+        />
+      </View>
     </Modal>
   );
 };
--- src/components/CreatePinModal/styles.js
+++ src/components/CreatePinModal/styles.js
@@ -2,25 +2,15 @@ import { StyleSheet } from "react-native";
 import colors from "../../styles/colors";
 
 export default StyleSheet.create({
-  container: {
-    alignItems: "center",
-    height: 300,
-    width: 300,
-    borderRadius: 5,
-  },
-  inputWrapper: {},
-  label: { marginBottom: 2 },
   input: {
     height: 100,
-    width: 250,
-    backgroundColor: colors.lightGrey,
+    width: "100%",
+    backgroundColor: colors.almostWhite,
+    borderRadius: 5,
     justifyContent: "flex-start",
     textAlignVertical: "top",
-    paddingLeft: 5,
-    marginBottom: 10,
-  },
-  buttonContainer: {
-    width: "100%",
-    alignItems: "flex-end",
+    paddingLeft: 8,
+    paddingTop: 8,
+    marginVertical: 20,
   },
 });
